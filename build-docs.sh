61531ee846c11bd96883c5003814965e9a35de22
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-03-01 10:00:39 +0100

    Update env var pointing to local documentation repo

--- build-docs.sh
+++ build-docs.sh
@@ -4,7 +4,7 @@ set -ex
 BUILD_DIR=build 
 GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | sed 's#/#_#g')
-TARGET_DIR=${TARGET_DIR:-$HOME/git/iam-docs.github.io}
+TARGET_DIR=${TARGET_DIR:-$HOME/git/docs}
 
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
